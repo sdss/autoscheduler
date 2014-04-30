0e20e95d5c52d497b9395b3ea559d956cc1687e0
bathompso <bathompso@D-108-179-167-200.dhcp4.washington.edu>
2014-04-30 16:05:53 +0000

    efficiency improvements

--- autoscheduler/apogee/observability.py
+++ autoscheduler/apogee/observability.py
@@ -9,10 +9,17 @@ def observability(apg, par, times, lengths):
 	obs_start = time()
 	apo = obs.Site(32.789278, -105.820278)
 	obsarr = np.zeros([len(apg), len(times)])
+
+	# Determine moon coordinates
+	mpos = []
+	for t in range(len(times)):
+		moonra, moondec = moonpos(times[t])
+		mpos.append(coo.ICRSCoordinates(moonra, moondec))
 	
 	# Loop over all plates
 	for p in range(len(apg)):
 		# Initalize obsarr row
+		if apg[p].priority <= 0: continue
 		for t in range(len(times)): obsarr[p,t] = apg[p].priority
 		
 		# Compute observing constants
@@ -21,26 +28,23 @@ def observability(apg, par, times, lengths):
 		if transitmjd - int(times[0]) > 1: transitmjd -= 1
 		if transitmjd - int(times[0]) < -1: transitmjd += 1
 		
-		# Gaussian prioritization on time from transit
-		for t in range(len(times)): 
+		for t in range(len(times)):
+			# Gaussian prioritization on time from transit
 			obsarr[p,t] += 50.0 * float(np.exp( -(transitmjd - times[t]+lengths[t]/2/24)**2 / (2 * (15)**2)))
+
+			# Moon avoidance
+			moondist = mpos[t] - apyscoo
+			if moondist.d < par['moon_threshold']:
+				obsarr[p,t] = -3
+				continue
 		
-		# Moon avoidance
-		for t in range(len(times)):
-			moonra, moondec = moonpos(times[t])
-			mooncoo = coo.ICRSCoordinates(moonra, moondec)
-			moondist = mooncoo - apyscoo
-			if moondist.d < par['moon_threshold']: obsarr[p,t] = -3
-		
-		# Determine whether HAs of block are within observational range
-		for t in range(len(times)):
-			if obsarr[p,t] < 0: continue
-		
-			if (transitmjd - times[t]) * 15 < apg[p].minha: obsarr[p,t] = -1
-			if (times[t]+lengths[t]/24 - transitmjd) * 15 > apg[p].maxha: obsarr[p,t] = -1
+			# Determine whether HAs of block are within observational range
+			if (transitmjd - times[t]) * 15 < apg[p].minha or (times[t]+lengths[t]/24 - transitmjd) * 15 > apg[p].maxha: 
+				obsarr[p,t] = -1
+				continue
 		
 			# Compute horiztonal coordinates
-			horz = apo.apparentCoordinates(apyscoo, datetime=[times[t] + lengths[t] / 5 / 24 * x for x in range(5)])
+			horz = apo.apparentCoordinates(apyscoo, datetime=[times[t] + lengths[t] / 2 / 24 * x for x in range(3)])
 			secz = [1/np.cos((90.0 - horz[x].alt.d) * np.pi / 180) for x in range(len(horz))]
 			# Check whether any of the points contain a bad airmass value
 			badsecz = [x for x in secz if x < 1.003 or x > par['maxz']]
