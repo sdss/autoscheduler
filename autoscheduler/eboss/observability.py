bfed7fa835f2388a4d434c30388085fa21058429
bathompso <bathompso@D-108-179-167-200.dhcp4.washington.edu>
2014-04-30 03:20:51 +0000

    beginning of eBOSS scheduling routine

---
+++ autoscheduler/eboss/observability.py
@@ -0,0 +1,50 @@
+from __future__ import print_function, division
+from time import time
+import numpy as np
+import astropysics.coords as coo
+import astropysics.obstools as obs
+from ..moonpos import moonpos
+
+def observability(ebo, par, times):
+	obs_start = time()
+	apo = obs.Site(32.789278, -105.820278)
+	obsarr = np.zeros([len(ebo), len(times)])
+	
+	# Loop over all plates
+	for p in range(len(ebo)):
+		# Initalize obsarr row
+		for t in range(len(times)): obsarr[p,t] = ebo[p].manual_priority * 100
+		
+		# Compute observing constants
+		apyscoo = coo.ICRSCoordinates(ebo[p].ra, ebo[p].dec)
+		transitmjd = obs.calendar_to_jd(apo.nextRiseSetTransit(apyscoo, dtime=obs.jd_to_calendar(times[0]))[2])
+		if transitmjd - int(times[0]) > 1: transitmjd -= 1
+		if transitmjd - int(times[0]) < -1: transitmjd += 1
+		
+		# Gaussian prioritization on time from transit
+		for t in range(len(times)): 
+			obsarr[p,t] += 50.0 * float(np.exp( -(transitmjd - times[t]+par['exposure']/2/24)**2 / (2 * (15)**2)))
+		
+		# Moon avoidance
+		for t in range(len(times)):
+			moonra, moondec = moonpos(times[t])
+			mooncoo = coo.ICRSCoordinates(moonra, moondec)
+			moondist = mooncoo - apyscoo
+			if moondist.d < par['moon_threshold']: obsarr[p,t] = -3
+		
+		# Determine whether HAs of block are within observational range
+		for t in range(len(times)):
+			if obsarr[p,t] < 0: continue
+		
+			if (transitmjd - times[t]) * 15 < ebo[p].minha: obsarr[p,t] = -1
+			if (times[t]+lengths[t]/24 - transitmjd) * 15 > ebo[p].maxha: obsarr[p,t] = -1
+		
+			# Compute horiztonal coordinates
+			horz = apo.apparentCoordinates(apyscoo, datetime=[times[t] + par['exposure'] / 3 / 24 * x for x in range(3)])
+			secz = [1/np.cos((90.0 - horz[x].alt.d) * np.pi / 180) for x in range(len(horz))]
+			# Check whether any of the points contain a bad airmass value
+			badsecz = [x for x in secz if x < 1.003 or x > par['maxz']]
+			if len(badsecz) > 0: obsarr[p,t] = -2
+	obs_end = time()
+	print("[PY] Determined eBOSS observability (%.3f sec)" % (obs_end - obs_start))
+	return obsarr
--- autoscheduler/eboss/schedule_eboss.py
+++ autoscheduler/eboss/schedule_eboss.py
@@ -1,55 +1,29 @@
 from __future__ import print_function, division
 from time import time
-import astropysics.coords as coo
-import astropysics.obstools as obs
-import os
+import numpy as np
+
+from get_eboss_plates import get_plates
 
 # SCHEDULE_EBOSS
 # DESCRIPTION: Main eBOSS scheduling routine.
 # INPUT: schedule -- dictionary defining important schedule times throughout the night
 # OUTPUT: eboss_choices -- dictionary list containing plate choices + observing times for tonight 
 def schedule_eboss(schedule, plan=False):
+	# Define eBOSS observing parameters
+	par = {'exposure': 16.5, 'ncarts': 8, 'maxz': 2.0, 'moon_threshold': 30, 'snr_avg': 9.0, 'snb_avg': 4.0, 'snr': 25, 'snb': 10}
+	
+	# Divide eBOSS time into blocks
+	times = np.arange(schedule['eboss_start'], schedule['eboss_end'], par['exposure']/60/24)
 
 	# Get all plate information from the database
-	ebo = get_eboss_plates(plan=plan)
+	ebo = get_plates(plan=plan)
 	
-	return ebo
+	# Determine observability
+	obs = observability(ebo, par, times)
 
 
-# GET_EBOSS_PLATES
-# DESCRIPTION: Reads in eBOSS plate information from platedb
-# INPUT: none
-# OUTPUT: ebo -- list of dicts with all eBOSS plate information
-def get_eboss_plates(plan=False):
-	# Create database connection
-	if (os.path.dirname(os.path.realpath(__file__))).find('utah.edu') >= 0: from sdss.internal.database.connections.UtahLocalConnection import db
-	else: from sdss.internal.database.connections.APODatabaseUserLocalConnection import db
-	session = db.Session()
-	apo = obs.Site(32.789278, -105.820278)
-	
-	ebo = []
-	
-	# Determine what is currently plugged
-	sql_start = time()
-	eboplug = session.execute("SET SCHEMA 'platedb'; "+
-		"SELECT crt.number, plt.plate_id, ptg.center_ra, ptg.center_dec "+
-		"FROM ((((((platedb.active_plugging AS ac "+
-			"JOIN platedb.plugging AS plg ON (ac.plugging_pk=plg.pk)) "+
-			"LEFT JOIN platedb.cartridge AS crt ON (plg.cartridge_pk=crt.pk)) "+
-			"LEFT JOIN platedb.plate AS plt ON (plg.plate_pk=plt.pk)) "+
-			"LEFT JOIN platedb.plate_to_survey AS p2s ON (p2s.plate_pk=plt.pk)) "+
-			"LEFT JOIN platedb.plate_pointing as pltg ON (pltg.plate_pk=plt.pk)) "+
-			"LEFT JOIN platedb.pointing AS ptg ON (pltg.pointing_pk=ptg.pk)) "+
-		"WHERE p2s.survey_pk = 2 ORDER BY crt.number").fetchall()
-	sql_end = time()
-	print("[SQL] Read in currently plugged eBOSS plates (%d sec)" % ((sql_end - sql_start)))
-	
-	for i in eboplug:
-		plate = coo.ICRSCoordinates(i[2], i[3])
-		transitmjd = obs.calendar_to_jd(apo.nextRiseSetTransit(plate)[2])
-		ebo.append({'plateid': i[1], 'transittime': transitmjd})
-		
-	return ebo
+
+
 		
 		
 	
