59b69df281be499a176d4f8c3091189b444ab161
bathompso <bathompso@D-108-179-167-200.dhcp4.washington.edu>
2014-04-30 14:49:51 +0000

    new logic for placing eboss plates

--- autoscheduler/eboss/pick_eboss_plates.py
+++ autoscheduler/eboss/pick_eboss_plates.py
@@ -12,7 +12,6 @@ def pick_plates(ebo, par, times, obs):
 	for p in range(len(ebo)):
 		if ebo[p].plugged == 0: continue
 		nleft = ebo[p].visleft(par)
-		print(nleft, max(obs[p,:]))
 		# Plate is complete or no longer observable, we can't keep it plugged
 		if nleft == 0 or max(obs[p,:]) < 0:
 			for t in range(len(times)): obs[p,t] = -10
@@ -21,14 +20,21 @@ def pick_plates(ebo, par, times, obs):
 		optslot = [x for x in range(len(times)) if obs[p,x] == max(obs[p,:])][0]
 		centerslot = int(round(nleft / 2)-1)
 		# Optimal slot is too close to beginning of night, start from beginning
-		if optslot < centerslot:
-			for i in range(nleft): chosen[i] = ebo[p].plateid
+		if optslot < centerslot: startslot = 0
 		# Optimal slot is too close to end of night, start from end
-		elif optslot > len(times)-nleft+centerslot:
-			for i in range(nleft): chosen[-i-1] = ebo[p].plateid
+		elif optslot > len(times)-nleft+centerslot: startslot = len(times) - nleft - 1
 		# Optimal range is wholly within the night, place it
-		else:
-			for i in range(nleft): chosen[optslot-centerslot+i] = ebo[p].plateid
+		else: startslot = optslot-centerslot
+		
+		# Determine whether any adjustments are necessary due to alread-plugged plates
+		if chosen[startslot] >= 0: startslot += 1
+		if chosen[startslot+nleft] >= 0: startslot -1
+		
+		# Mark slots as chosen
+		for i in range(nleft):
+			if startslot+i >= len(chosen): continue
+			chosen[startslot+i] = ebo[p].plateid
+		# Remove plate from further picking
 		for t in range(len(times)): obs[p,t] = -10
 	pickplug_end = time()
 	print("[PY] Placed eBOSS already-plugged plates (%.3f sec)" % (pickplug_end - pickplug_start))
