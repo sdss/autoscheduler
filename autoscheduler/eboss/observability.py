a52274863210e68cdbaa7a1c7a423f78ff44c0bc
bathompso <bathompso@D-108-179-167-200.dhcp4.washington.edu>
2014-06-23 16:16:23 +0000

    updated observability routine for new LST determination

--- autoscheduler/eboss/observability.py
+++ autoscheduler/eboss/observability.py
@@ -9,6 +9,8 @@ def observability(ebo, par, times, loud=True):
     obs_start = time()
     apo = obs.Site(32.789278, -105.820278)
     obsarr = np.zeros([len(ebo), len(times)])
+    beglst = [apo.localSiderialTime(x) for x in times]
+    endlst = [apo.localSiderialTime(times[x] + par['exposure']/24) for x in range(len(times))]
     
     # Determine moon coordinates
     mpos = []
@@ -22,28 +24,28 @@ def observability(ebo, par, times, loud=True):
         for t in range(len(times)): obsarr[p,t] = ebo[p].manual_priority * 100
         
         # Compute observing constants
-        apyscoo = coo.ICRSCoordinates(ebo[p].ra, ebo[p].dec)
-        transitmjd = obs.calendar_to_jd(apo.nextRiseSetTransit(apyscoo, dtime=obs.jd_to_calendar(times[0]))[2])
-        if transitmjd - int(times[0]) > 1: transitmjd -= 1
-        if transitmjd - int(times[0]) < -1: transitmjd += 1
+        platecoo = coo.ICRSCoordinates(ebo[p].ra, ebo[p].dec)
+        platelst = float(ebo[p].ra + ebo[p].ha) / 15
+        minlst = float(ebo[p].ra + ebo[p].minha) / 15
+        maxlst = float(ebo[p].ra + ebo[p].maxha) / 15
         
         for t in range(len(times)): 
             # Gaussian prioritization on time from transit
-            obsarr[p,t] += 50.0 * float(np.exp( -(transitmjd - times[t]+par['exposure']/2/24)**2 / (2 * (15)**2)))
+            obsarr[p,t] += 50.0 * float(np.exp( -(platelst - (beglst[t]+endlst[t])/2 + par['exposure']/2)**2 / 2))
         
             # Moon avoidance
-            moondist = mpos[t] - apyscoo
-            if moondist.d < par['moon_threshold']: 
+            moondist = mpos[t] - platecoo
+            if moondist.d < par['moon_threshold']:
                 obsarr[p,t] = -3
                 continue
         
             # Determine whether HAs of block are within observational range
-            if (transitmjd - times[t]) * 15 < ebo[p].minha or (times[t]+par['exposure']/24 - transitmjd) * 15 > ebo[p].maxha:
+            if beglst[t] < minlst or endlst[t] > maxlst: 
                 obsarr[p,t] = -1
                 continue
         
             # Compute horiztonal coordinates
-            horz = apo.apparentCoordinates(apyscoo, datetime=times[t] + par['exposure'] / 2 / 24)
+            horz = apo.apparentCoordinates(platecoo, datetime=times[t] + par['exposure'] / 2 / 24)
             secz = 1/np.cos((90.0 - horz[0].alt.d) * np.pi / 180)
             # Check whether any of the points contain a bad airmass value
             if secz < 1.003 or secz > par['maxz']: obsarr[p,t] = -2
