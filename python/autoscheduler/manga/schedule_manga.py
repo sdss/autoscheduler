3f3d38e2a727aea8afbc2e6fe5cb5d574ca344be
bathompso <bathompso@D-108-179-167-200.dhcp4.washington.edu>
2014-07-25 16:30:56 +0000

    updated MaNGA wrapper

--- python/autoscheduler/manga/schedule_manga.py
+++ python/autoscheduler/manga/schedule_manga.py
@@ -1,7 +1,7 @@
 from __future__ import print_function, division
 from autoscheduler.manga.Totoro.scheduler import Nightly
 
-def schedule_manga(schedule, errors, plan=plan, loud=loud):
+def schedule_manga(schedule, errors, plan=False, loud=True):
 	# Get raw output from MaNGA submodule
 	manga_obj = Nightly(startDate=schedule['man_start'], endDate=schedule['man_end'])
 	manga_output = manga_obj.getOutput()
