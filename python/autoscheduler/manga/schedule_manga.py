5ced701aefc29c98a7d51da4e8a19f614528944a
bathompso <bathompso@D-108-179-167-200.dhcp4.washington.edu>
2014-08-21 02:24:33 +0000

    updated MaNGA output

--- python/autoscheduler/manga/schedule_manga.py
+++ python/autoscheduler/manga/schedule_manga.py
@@ -6,5 +6,45 @@ def schedule_manga(schedule, errors, plan=False, loud=True):
 	manga_obj = Nightly(startDate=schedule['manga_start'], endDate=schedule['manga_end'])
 	manga_output = manga_obj.getOutput()
 	
-	return manga_output
+	# Massage the MaNGA 'plates' output into a usable format
+	plates = []
+	for plate_id, plate_data in manga_output['plates'].iteritems():
+		plate_dict = {}
+		plate_dict['plateid'] = plate_id
+		plate_dict['complete'] = plate_data['complete']
+		plate_dict['HARange'] = list(plate_data['HARange'])
+		plate_dict['SN2'] = list(plate_data['SN2'])
+		# Completed Sets for this plate
+		plate_dict['sets'] = []
+		for set_pk, set_data in plate_data['sets'].iteritems():
+			set_dict = {}
+			set_dict['setpk'] = set_pk
+			set_dict['complete'] = set_data['complete']
+			set_dict['averageSeeing'] = set_data['averageSeeing']
+			set_dict['SN2'] = list(set_data['SN2'])
+			set_dict['SN2Range'] = [list(set_data['SN2Range'][0]), list(set_data['SN2Range'][1])]
+			set_dict['seeingRange'] = list(set_data['seeingRange'])
+			set_dict['HARange'] = list(set_data['HARange'])
+			set_dict['missingDithers'] = list(set_data['missingDithers'])
+			set_dict['exposures'] = []
+			# Exposures contained within dither set
+			for exp_pk, exp_data in set_data['exposures'].iteritems():
+				exp_dict = {}
+				exp_dict['exp_pk'] = exp_pk
+				exp_dict['valid'] = exp_data['valid']
+				exp_dict['ditherPosition'] = exp_data['ditherPosition']
+				exp_dict['HARange'] = list(exp_data['obsHARange'])
+				exp_dict['seeing'] = exp_data['seeing']
+				exp_dict['SN2'] = list(exp_data['SN2'])
+				set_dict['exposures'].append(exp_dict)
+			plate_dict['sets'].append(set_dict)
+		plates.append(plate_dict)
+		
+	manga_formatted = {'plates': plates, 'schedule': []}
+	
+	return manga_formatted
+	
+	
+	
+	
 	
