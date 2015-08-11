34e255d031fd6558faa120d91e9d99564df02d85
ndelee <ndelee@D-108-179-167-200.dhcp4.washington.edu>
2015-08-11 22:44:35 +0000

    Updated how slots, times and lengths work with twilight.

--- python/autoscheduler/apogee/schedule_apogee.py
+++ python/autoscheduler/apogee/schedule_apogee.py
@@ -11,44 +11,53 @@ from pick_apogee_plates import pick_plates
 # INPUT: schedule -- dictionary defining important schedule times throughout the night
 # OUTPUT: apogee_choices -- dictionary list containing plate choices + observing times for tonight 
 def schedule_apogee(schedule, errors, plan=False, loud=True, twilight=False):
-	#define twilight parameters
+
+	# Define APOGEE-II observing parameters
+        par = {'exposure': 67, 'overhead': 20, 'ncarts': 9, 'maxz': 3, 'moon_threshold': 15, 'sn_target': 3136}
+
+        # Define APOGEE-II blocks for tonight
+        nightlength = (schedule['bright_end'] - schedule['bright_start']) * 24
+        nslots = min([int(round(nightlength / ((par['exposure'] + par['overhead']) / 60))), par['ncarts']])
+        if nslots != 0:
+                times = [schedule['bright_start'] + (par['exposure'] + par['overhead']) / 60 / 24 * x for x in range(nslots)]
+                lengths = [(par['exposure'] + par['overhead']) / 60 for x in range(nslots)]
+		
+                # If APOGEE-II starts the split night
+                if schedule['bright_start'] < schedule['dark_start'] or schedule['dark_start'] == 0:
+                        # Determine whether we should add another exposure (leftover time > 15min)
+                        if len(times) < par['ncarts'] and nightlength - sum(lengths) > 0:
+                                times.append(schedule['bright_start'] + len(times) * (par['exposure'] + par['overhead']) / 60 / 24)
+                                lengths.append(nightlength - sum(lengths))
+                        # Because APOGEE-II is first, the last exposure will not have overhead
+                        else: lengths[-1] = par['exposure'] / 60
+		
+                # APOGEE-II ends the night
+                else:
+                        # Determine whether we can add an exposure (leftover time > 15min)
+                        if len(times) < par['ncarts'] and nightlength - sum(lengths) > par['overhead'] / 60:
+                                times.append(schedule['bright_start'] + len(times) * (par['exposure'] + par['overhead']) / 60 / 24)
+                                lengths.append(nightlength - sum(lengths))
+
+        else:
+                times = []
+                lengths = []
+        #define twilight parameters
 	if twilight:
 		par = {'exposure': 67, 'overhead': 20, 'ncarts': 9, 'maxz': 3, 'moon_threshold': 15, 'sn_target': 3136}
 
 		# Define APOGEE-II blocks for tonight
 		if schedule['manga_end'] > schedule['eboss_end']: 
-			times= [schedule['manga_end']+(par['exposure'] + par['overhead']) / 60 / 24]
+			twtimes= [schedule['manga_end']+(par['exposure'] + par['overhead']) / 60 / 24]
 		else:
-			times= [schedule['eboss_end']+(par['exposure'] + par['overhead']) / 60 / 24]
-		lengths = [(par['exposure'] + par['overhead']) / 60]
-		#temp result until we want to really test twilight
-		return []
-	# Define APOGEE-II observing parameters
-	else:
-		par = {'exposure': 67, 'overhead': 20, 'ncarts': 9, 'maxz': 3, 'moon_threshold': 15, 'sn_target': 3136}
+			twtimes= [schedule['eboss_end']+(par['exposure'] + par['overhead']) / 60 / 24]
+		twlengths = [(par['exposure'] + par['overhead']) / 60]
+                        
+                times = times + twtimes
+                lenghts = lengths + twlengths
+                nslots = nslots + 1
 
-		# Define APOGEE-II blocks for tonight
-		nightlength = (schedule['bright_end'] - schedule['bright_start']) * 24
-		nslots = min([int(round(nightlength / ((par['exposure'] + par['overhead']) / 60))), par['ncarts']])
-		if nslots == 0: return []
-		times = [schedule['bright_start'] + (par['exposure'] + par['overhead']) / 60 / 24 * x for x in range(nslots)]
-		lengths = [(par['exposure'] + par['overhead']) / 60 for x in range(nslots)]
-		
-		# If APOGEE-II starts the split night
-		if schedule['bright_start'] < schedule['dark_start'] or schedule['dark_start'] == 0:
-			# Determine whether we should add another exposure (leftover time > 15min)
-			if len(times) < par['ncarts'] and nightlength - sum(lengths) > 0:
-				times.append(schedule['bright_start'] + len(times) * (par['exposure'] + par['overhead']) / 60 / 24)
-				lengths.append(nightlength - sum(lengths))
-			# Because APOGEE-II is first, the last exposure will not have overhead
-			else: lengths[-1] = par['exposure'] / 60
-		
-		# APOGEE-II ends the night
-		else:
-			# Determine whether we can add an exposure (leftover time > 15min)
-			if len(times) < par['ncarts'] and nightlength - sum(lengths) > par['overhead'] / 60:
-				times.append(schedule['bright_start'] + len(times) * (par['exposure'] + par['overhead']) / 60 / 24)
-				lengths.append(nightlength - sum(lengths))
+        #Return nothing if no APOGEE slots needed.
+        if nslots == 0: return []
 
 	# Get all plate information from the database
 	apg = get_plates(errors, plan=plan, loud=loud)
